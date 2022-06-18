2aaf69f2fac190164f058ff58c785d9d506b892a
Karan <karan42693@gmail.com>
2022-06-18 18:55:09 +0530

    added top loading bar and api key to env

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import NewsItems from './NewsItems'
-import Articles from './sampleOutput'
+// import Articles from './sampleOutput'
 import Spinner from './Spinner'
 import PropTypes from 'prop-types'
 import InfiniteScroll from "react-infinite-scroll-component";
@@ -35,15 +35,19 @@ export class News extends Component {
     }
 
     async updateNews() {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=491cf8ec981442519a54f16bffee8047&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.props.setProgress(10);
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true })
         let data = await fetch(url);
+        this.props.setProgress(40);
         let parsedData = await data.json()
+        this.props.setProgress(80);
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false
         })
+        this.props.setProgress(100);
     }
 
     // life cycle method --> (componentDidMount)
@@ -53,7 +57,7 @@ export class News extends Component {
 
     fetchMoreData = async () => {
         this.setState({ page: this.state.page + 1 });
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=491cf8ec981442519a54f16bffee8047&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json()
         this.setState({
